1f502394f3d5c313557be33f692105709ffb2346
HernandezPy <douglasore14@gmail.com>
2024-06-14 15:03:55 +0000

    Fri, Jun 14, 2024, 11:03 AM -04:00

--- indoor/playback.py
+++ indoor/playback.py
@@ -1,3 +1,3 @@
-print("Thi...is...cs50")
-print("Thi...is...our...week...on...funtions")
+print("This...is...cs50")
+print("This...is...our...week...on...funtions")
 print("Let's...implement...a...function...called...hello")
