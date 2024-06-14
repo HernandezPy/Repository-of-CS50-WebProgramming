b6cad65712a27671392407d8aaa603bf80e379a5
HernandezPy <douglasore14@gmail.com>
2024-06-14 15:18:02 +0000

    Fri, Jun 14, 2024, 11:18 AM -04:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,3 +1,3 @@
-print("This is CS50")
-print("This is our week on funtions")
-print("Let's implement a funtione called hello")
+print("This...is...CS50")
+print("This...is...our...week...on...funtions")
+print("Let's...implement...a...funtione...called...hello")
