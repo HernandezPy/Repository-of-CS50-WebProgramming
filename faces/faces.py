1cf0ab6624be66c74be686177369eb15277c01bd
HernandezPy <douglasore14@gmail.com>
2024-06-17 22:42:51 +0000

    Mon, Jun 17, 2024, 6:42 PM -04:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,12 +1,8 @@
-def lowercase(input_lower):
-    user_str = input_lower.case("HELLO, wORD").lower.case("hello,word")
-    return user_str
-
 def main():
-    lower_case = input('PLEASE ENTER ONLY CAPITAL LETTERS: ')
-    capital_letter = lower_case
-    print('capital_letter')
+    user_input = input("Please enter your text: ")
+    lowercased_text = user_input.lower()
+    print(lowercased_text)
 
-if __name__ == '__main__':
 
-  main()
+if __name__ == "__main__":
+    main()
