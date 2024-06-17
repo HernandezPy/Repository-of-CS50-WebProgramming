1c6d6a7f47596064b63284a6f80822ab96e83fdd
HernandezPy <douglasore14@gmail.com>
2024-06-17 22:16:00 +0000

    Mon, Jun 17, 2024, 6:16 PM -04:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,3 +1,17 @@
 print('Hello 🙂')
 print('Goodbye :(')
 print('Hello 🙂 Goodbye 🙁')
+
+def convert(input_str):
+    emoji_str = input_str.replace(':)', '🙂').replace(':(', '🙁')
+    return emoji_str
+
+
+def main():
+    user_emoji = input('Please enter your text: ')
+    converted_text = convert(user_emoji)
+    print(converted_text)
+
+if __name__ == '__main__':
+
+   main()
