95e5a9cb8dd33045920d8ed1792bac72d5046dcd
HernandezPy <douglasore14@gmail.com>
2024-06-18 14:35:55 +0000

    Tue, Jun 18, 2024, 10:35 AM -04:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -2,8 +2,7 @@ def main():
     speed_of_light = 300000000
     mass_kg = int(input("mass: "))
     measured_in_Juoles = mass_kg * speed_of_light ** 2
-
-    print(f"E: ")
+    print(f"E: {measured_in_Joules} ")
 
 if __name__ == "__main__":
-main()
+  main()
