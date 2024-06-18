2a51c7f5eb9b53e56ded3c7257c9f6c6991c3558
HernandezPy <douglasore14@gmail.com>
2024-06-18 14:32:36 +0000

    Tue, Jun 18, 2024, 10:32 AM -04:00

---
+++ einstein/einstein.py
@@ -0,0 +1,9 @@
+def min():
+    speed_of_light = 300000000
+    mass_kg = int(input("mass: "))
+    measured_in_Juoles = mass_kg * speed_of_light ** 2
+
+    print(f"E: ")
+
+if __name__ == "__main__":
+    main()
