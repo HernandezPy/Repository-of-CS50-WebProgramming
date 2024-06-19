f8adcdca6db1122ae5f09f3665a007a5c738f066
HernandezPy <douglasore14@gmail.com>
2024-06-19 23:08:58 +0000

    Wed, Jun 19, 2024, 7:08 PM -04:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,7 @@
 def main(answer):
     normalized_answer = answer.lower().strip()
     normal_answer = ['42', 'forty-two', 'forty two']
-    if normalized_answer == normal_answer:
+    if normalized_answer in normal_answer:
       return 'yes'
     else:
        return 'no'
