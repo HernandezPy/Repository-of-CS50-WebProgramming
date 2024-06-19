fe67a3eee14e070f8a1cbc12271b0aad969106f1
HernandezPy <douglasore14@gmail.com>
2024-06-19 22:05:51 +0000

    Wed, Jun 19, 2024, 6:05 PM -04:00

--- deep/deep.py
+++ deep/deep.py
@@ -2,18 +2,14 @@ print("Este es Deep Thought")
 print("Bienvenido a mi juego de preguntas!")
 
 question = input("What is the answer to the great question of life, the universe, and everything?")
+while True:
+       answer == answer
 
-def question():
-   question.input = question
-
-
-if answer == ('42'):
-   print("yes")
-elif answer == ('forty-two'):
-    print('yes')
-elif answer == ('forty two'):
-    print('yes')
-else:
+if answer == ('42')
+      print("yes")
+    elif answer == ('forty-two')
+        print('yes')
+     elif answer == ('forty two')
+         print('yes')
+   else:
    print('no')
-
-question()
