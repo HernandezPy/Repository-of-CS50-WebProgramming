05c549c313773180dcb60c225e316094b9d9d289
HernandezPy <douglasore14@gmail.com>
2024-06-19 23:04:09 +0000

    Wed, Jun 19, 2024, 7:04 PM -04:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,7 @@
 def main(answer):
     normalized_answer = answer.lower().strip()
     normal_answer = ['42', 'forty-two', 'forty two']
-    if normalized_answer = normal_answer:
+    if normalized_answer == normal_answer:
       return 'yes'
     else:
        return 'no'
@@ -11,4 +11,8 @@ def check_question():
    question = input("What is the Great Question of life, the Universe, and everythin? ")
    result = main(check_question)
    print(result)
+
+
+if __name__ == "__main_":
+   main()
 
