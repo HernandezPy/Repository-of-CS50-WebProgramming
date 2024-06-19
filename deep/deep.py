0043f8d4b36f5134c56fa4e89d909341f5ce38e8
HernandezPy <douglasore14@gmail.com>
2024-06-19 22:02:45 +0000

    Wed, Jun 19, 2024, 6:02 PM -04:00

--- deep/deep.py
+++ deep/deep.py
@@ -3,12 +3,17 @@ print("Bienvenido a mi juego de preguntas!")
 
 question = input("What is the answer to the great question of life, the universe, and everything?")
 
+def question():
+   question.input = question
+
 
 if answer == ('42'):
    print("yes")
-   elif answer == ('forty-two'):
-      print('yes')
-   elif answer == ('forty two'):
-      print('yes')
+elif answer == ('forty-two'):
+    print('yes')
+elif answer == ('forty two'):
+    print('yes')
 else:
    print('no')
+
+question()
