7ef332584d247ff88d8d9b8024cefcbb5b72e026
HernandezPy <douglasore14@gmail.com>
2024-06-20 15:16:07 +0000

    Thu, Jun 20, 2024, 11:16 AM -04:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,1 +1,25 @@
-def greeting
+def greeting_response(hello):
+    greeting = hello.strip().lower()
+    if greeting_response == 'hello':
+        return 0
+    elif greeting_response == 'h':
+        return 20
+    else:
+        return 100
+
+
+def main():
+    while True:
+        user_greeting = input("Greeting (or type 'exit' to quit): ")
+        if user_greeting == 'exit':
+            print('Exiting the program')
+            break
+        result = greeting_response(user_greeting)
+        print(f'{result}')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
