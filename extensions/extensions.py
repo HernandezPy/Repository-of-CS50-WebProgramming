11fb501c1accf4ebedee6921a7f541308b8bdc83
HernandezPy <douglasore14@gmail.com>
2024-06-21 16:47:51 +0000

    Fri, Jun 21, 2024, 12:47 PM -04:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,4 +3,15 @@ def extensions():
     valid_extensions = {
         ['.gif', '.jpg', '.jpeg', '.png', '.pdf', '.txt', '.zip']
     }
-    if insen
+    if valid_extensions in insensitively:
+        print(f'image/{valid_extensions})
+              else:
+              print('application/octet-stream')
+
+
+def valid_extensions():
+    answer_input = input('File name: ')
+    result = extensions(answer_input)
+
+if __name__ == '__main__':
+ main()
