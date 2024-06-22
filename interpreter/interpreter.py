ae4a18c9293f53fbb588141486b0dbec0bb29d71
HernandezPy <douglasore14@gmail.com>
2024-06-22 15:21:36 +0000

    Sat, Jun 22, 2024, 11:21 AM -04:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,3 +1,9 @@
 def get_interpreter(arithmetic):
     user_input = arithmetic.float().strip()
-    
+    x = is_an_integer
+    y = ['+', '-', '*', '/']
+    z = is_an_integer
+    x, y, z = expresions.split(' ')
+
+def values(x, y, z)
+
