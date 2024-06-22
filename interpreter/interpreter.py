68b80225d9250958d02f924f2e0f1a56d8f3276f
HernandezPy <douglasore14@gmail.com>
2024-06-22 15:39:56 +0000

    Sat, Jun 22, 2024, 11:39 AM -04:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,10 +1,32 @@
-def get_interpreter(arithmetic):
-    x, y, z = expresions.split(' ').float()
-    x = is_an_integer
-    y = ['+', '-', '*', '/']
-    z = is_an_integer
+def interpreter_expression(expression):
+    try:
+        x, y, z, = expression.split()
+
+        x = int(x)
+        y = int(y)
+
+        if y == '+':
+            return x + z
+        elif y == '-':
+            return x - z
+        elif y == '*':
+            return x * z
+        elif y == '/':
+            return x / z
+        else:
+            return 'Error: Invalid operator.'
+        return f'{result:.1f}'
+
+    except ValueError:
+        return "Error: Invalid input. Ensure the input is  formatted as 'x y z' where x and z are integers and y an operator (+,-,*,/).'
+    except ZeroDivisionError:
+        return 'Error: Division with zero is not allowed.'
+
+    def main():
+        expression = input('Expression: ').strip()
+        result = interpreter_expression(expression)
+        print(result)
 
 
-def values(x, y, z):
-    user_input = input(Expresion: ')
-    is_an_integer = 
+    if __name__ == '__main__':
+        main()
