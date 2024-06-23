14977ecbd9e806bc62963c24763d10beb2af5f78
HernandezPy <douglasore14@gmail.com>
2024-06-23 16:47:35 +0000

    Sun, Jun 23, 2024, 12:47 PM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,21 +1,21 @@
 
 def convert(time):
-    
-    hours, minutes = map(int, time.split(':'))
+
+    hours, minutes = time.split(":")
     return hours + minutes / 60
 
 def main():
-
-    time = input("Enter the time in 24-hour format (HH:MM or H:MM): ").strip()
-
+    time = input('What time is it? ').strip()
     hours = convert(time)
 
     if 7 <= hours <= 8:
-        print("It's breakfast time.")
+       print('breakfast time')
     elif 12 <= hours <= 13:
-        print("It's lunch time.")
+       print('lunch time')
     elif 18 <= hours <= 19:
-        print("It's dinner time.")
+       print('dinner time')
+    else:
+       pass
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+ main()
