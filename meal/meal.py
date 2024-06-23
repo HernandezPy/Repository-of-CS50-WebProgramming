357a4a791e8c2a3aeaa99c95e13c1e6e4eee4996
HernandezPy <douglasore14@gmail.com>
2024-06-23 16:01:11 +0000

    Sun, Jun 23, 2024, 12:01 PM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,6 +1,6 @@
 def main():
 
-    time = {
+    meals_time = {
     'breakfast_time':'7:00am between 8:00am'
     'lunch_time': '12:00am between 13:00pm'
     'dinner_time': '18:00pm between 19:00pm'
@@ -8,6 +8,13 @@ def main():
     }
 
      hours, minutes = time.split(':')
-    for time, in hours_time:
+
+    for meals_time, in hours_time:
        if breakfast_time == hours_time:
-          return 
+          return breakfast time
+       elif lunch_time == hours_tiem:
+          return lunch time
+       elif dinner_time == hours_time:
+          return dinner time
+       else:
+          pass
