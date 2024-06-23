7fb56203170bdb08d8a0d8cfaacd842b52e75279
HernandezPy <douglasore14@gmail.com>
2024-06-23 15:17:03 +0000

    Sun, Jun 23, 2024, 11:17 AM -04:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -18,9 +18,9 @@ def interpreter_expression(expression):
             return 'Error: Invalid operator.'
         return f'{result:.1f}'
 
-    except ValueError:
+     except ValueError:
         return "Error: Invalid input. Ensure the input is  formatted as 'x y z' where x and z are integers and y an operator (+,-,*,/)."
-    except ZeroDivisionError:
+     except ZeroDivisionError:
         return 'Error: Division with zero is not allowed.'
 
 
