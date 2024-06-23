5835d666c241b804ef7800f8391922ffbc24490c
HernandezPy <douglasore14@gmail.com>
2024-06-23 15:53:18 +0000

    Sun, Jun 23, 2024, 11:53 AM -04:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,10 @@
 def main():
+
     hours, minutes = time.split(':')
 
-    
-    breakfast_time = [7:00am between 8:00am]
-    lunch_time = [12:00am between 13:00pm]
-    dinner_time = [18:00pm between 19:00pm]
+    time = {
+    'breakfast_time':'7:00am between 8:00am'
+    'lunch_time': '12:00am between 13:00pm'
+    'dinner_time': '18:00pm between 19:00pm'
+
+    }
