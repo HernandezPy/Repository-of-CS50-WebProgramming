5e1ef41401dab2e3595bb3c7cfe722f339675416
HernandezPy <douglasore14@gmail.com>
2024-06-26 23:12:17 +0000

    Wed, Jun 26, 2024, 7:12 PM -04:00

--- meal/itune.py
+++ meal/itune.py
@@ -5,8 +5,10 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get('https://itunes.apple.com/search?entity=song&limit=10&term=' + sys.argv[1])
+response = requests.get(
+    "https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1]
+)
 
 o = response.json()
-for result in o ["results"]:
+for result in o["results"]:
     print(result["trackName"])
