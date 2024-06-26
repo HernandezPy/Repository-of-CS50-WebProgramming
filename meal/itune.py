c1b4b5fce762032556bf8c8a2105acaa96c114e4
HernandezPy <douglasore14@gmail.com>
2024-06-26 22:55:24 +0000

    Wed, Jun 26, 2024, 6:55 PM -04:00

--- meal/itune.py
+++ meal/itune.py
@@ -6,4 +6,7 @@ if len(sys.argv) != 2:
     sys.exit()
 
 response = requests.get('https://itunes.apple.com/search?entity=song&limit=1&term=' + sys.argv[1])
-print(json.dumps(response.json(), indent=2))
+
+o = response.json()
+for result in o['result']:
+    print(result['trackName'])
