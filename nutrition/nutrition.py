32d7008f1cb7f3271481ba0a8ce896b1f5455008
HernandezPy <douglasore14@gmail.com>
2024-07-02 03:01:31 +0000

    Mon, Jul 1, 2024, 11:01 PM -04:00

---
+++ nutrition/nutrition.py
@@ -0,0 +1,43 @@
+fruits_calories = {
+
+    'Apples': 130,
+    'Avocado': 50,
+    'Bananas': 110,
+    'Cantaloupe': 50,
+    'Grapefruit': 60,
+    'Grapes': 90,
+    'Honeydew Melon': 50,
+    'Kiwifruit': 90,
+    'Lemon': 15,
+    'Nectarine': 60,
+    'Orenge': 80,
+    'Peach': 60,
+    'Pear': 100,
+    'Plums': 70,
+    'Strawberries': 50,
+    'Sweet Cherries': 100,
+    'Tangerine': 50,
+    'Watermelon': 80
+    
+  }
+
+def main():
+    fruit = input('Item: ').strip().lower()
+    if fruit in fruits_calories:
+         print(f'Calories: {fruits_calories[fruit]}')
+    else:
+        pass
+
+if __name__ == '__main__':
+ main()
+}
+
+def main():
+ fruit = input('Item: ').strip().lower()
+ if fruit in fruits_calories:
+ print(f'Calories: {fruits_calories[fruit]}')
+ else:
+    pass
+
+if __name__ == '__main__':
+ main()
