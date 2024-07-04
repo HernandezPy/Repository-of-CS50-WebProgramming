745464d93e6607b818ff8df92a89573368377386
HernandezPy <douglasore14@gmail.com>
2024-07-04 14:35:49 +0000

    Thu, Jul 4, 2024, 10:35 AM -04:00

---
+++ fuel/fuel.py
@@ -0,0 +1,29 @@
+def get_fuel_fraction():
+    while True:
+        try:
+            fraction = input('Fraction: ')
+            x, y = fraction.split('/')
+            x = int(x)
+            y = int(y)
+            if y == 0:
+                raise ZeroDivisionError
+            if x > y:
+                raise ValueError
+            percentage = (x / y) * 100
+            return percentage
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+def main():
+    percentage = get_fuel_fraction()
+    if percentage <= 1:
+        print('E')
+    elif percentage >= 99:
+        print('F')
+    else:
+        print(f'{round(percentage)}%')
+
+
+if __name__ == '__main__':
+    main()
