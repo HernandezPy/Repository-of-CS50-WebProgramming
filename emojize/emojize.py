e10120fef3409886c08eb53f1b0d7ba2c32b9e8e
HernandezPy <douglasore14@gmail.com>
2024-07-24 23:53:47 +0000

    Wed, Jul 24, 2024, 7:53 PM -04:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -7,7 +7,7 @@ def emojize_text(text):
 def main():
     user_input = input('Input: ')
     emojized_text = emojize_text(user_input)
-    print('Output: ', emojize_text)
+    print('Output:', emojized_text)
 
 
 if __name__ == '__main__':
