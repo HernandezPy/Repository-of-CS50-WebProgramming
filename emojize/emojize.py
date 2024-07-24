ab4cdd06c05f13f45f1749f72d6d053e518f0d05
HernandezPy <douglasore14@gmail.com>
2024-07-24 23:49:20 +0000

    Wed, Jul 24, 2024, 7:49 PM -04:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,1 +1,14 @@
-import emoji 
+import emoji
+
+def emojize_text(text):
+    return emoji.emojize(text, language='alias')
+
+
+def min():
+    user_input = input('Input: ')
+    emojized_text = emojize_text(user_input)
+    print('Output: ', emojize_text)
+
+
+if __name__ == '__main__':
+    main()
