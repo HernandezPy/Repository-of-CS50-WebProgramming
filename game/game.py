41201fef43927ae786942a3a65b360ee903cfa8a
HernandezPy <douglasore14@gmail.com>
2024-08-06 14:19:30 +0000

    Tue, Aug 6, 2024, 10:19 AM -04:00

---
+++ game/game.py
@@ -0,0 +1,37 @@
+import random
+
+def get_positive_integer(prompt):
+
+    while True:
+        try:
+            value = int(input(prompt))
+            if value > 0:
+                return value
+        except ValueError:
+            print("Please enter a positive integer.")
+
+
+def main():
+    while True:
+        level = get_positive_integer("Level: ")
+        if level > 0:
+            break
+
+    number_to_guess = random.randint(1, level)
+
+    while True:
+        guess = get_positive_integer("Guess: ")
+
+        if guess < number_to_guess:
+            print("Too small!")
+        elif guess > number_to_guess:
+            print("Too large!")
+        else:
+            print("Just right!")
+            break
+
+
+if __name__ == "__main__":
+    main()
+
+
