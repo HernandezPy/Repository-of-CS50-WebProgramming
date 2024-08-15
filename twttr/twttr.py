8adc3e50765fec646f4c181ca76fc8fc1857aa7c
HernandezPy <douglasore14@gmail.com>
2024-08-15 16:07:30 +0000

    Thu, Aug 15, 2024, 12:07 PM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,10 +2,10 @@ def main():
     user_input = input('input: ').strip()
     print(remove_vowels(user_input))
 
-    
-def remove_vowels(input_str):
+
+def shorten(word):
     vowels = ['a', 'e', 'i', 'o', 'u']
-    result = ''.join([char for char in input_str if char.lower() not in vowels])
+    result = ''.join([char for char in word if char.lower() not in vowels])
     return result
 
 if __name__ == '__main__':
