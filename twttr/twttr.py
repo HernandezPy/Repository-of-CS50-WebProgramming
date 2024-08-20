e49b8839b720f1bc4fd86a5812ddb11352b0fd45
HernandezPy <douglasore14@gmail.com>
2024-08-20 14:54:46 +0000

    Tue, Aug 20, 2024, 10:54 AM -04:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -5,7 +5,7 @@ def main():
 
 def shorten(word):
     vowels = ['a', 'e', 'i', 'o', 'u']
-    result = ''.join([char for char in word if char.lower() not in vowels])
+    result = ''.join([char for char in word if not in vowels])
     return result
 
 if __name__ == '__main__':
