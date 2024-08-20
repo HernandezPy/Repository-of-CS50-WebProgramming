a8b145d55373a2325e46ca8d0c28ff41c20e10a9
HernandezPy <douglasore14@gmail.com>
2024-08-20 14:48:52 +0000

    Tue, Aug 20, 2024, 10:48 AM -04:00

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -3,5 +3,12 @@ import pytest
 from twttr import shorten
 
 
-def text_shorten():
-    assert(shorten) == [char for char in word if char.lower() not in vowels]
+def test_shorten_lowercase():
+    assert shorten("hello") == "hll"
+    assert shorten("twitter") == "twttr"
+    assert shorten("apple") == "ppl"
+
+def test_shorten_uppercase():
+    assert shorten("HELLO") == "HLL"
+    assert shorten("TWITTER") == "TWTTR"
+    assert shorten("APPLE") == "PPL"
