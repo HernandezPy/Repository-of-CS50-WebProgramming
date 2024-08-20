0e44fa74d6d0864ec0338cf58df704a76903462d
HernandezPy <douglasore14@gmail.com>
2024-08-20 12:07:27 +0000

    Tue, Aug 20, 2024, 8:07 AM -04:00

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -2,9 +2,6 @@ import pytest
 
 from twttr import shorten
 
-def test_shorten():
-    assert() == ['a', 'e', 'i', 'o', 'u']
-
 
 def text_shorten():
     assert(shorten) == [char for char in word if char.lower() not in vowels]
