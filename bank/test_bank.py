578fc120d7b1d50402457494e030b95ea49d9374
HernandezPy <douglasore14@gmail.com>
2024-08-22 16:35:40 +0000

    Thu, Aug 22, 2024, 12:35 PM -04:00

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -5,3 +5,13 @@ def test_value_greeting():
     assert value("hello") == 0
     assert value("hey") == 20
     assert value("goodmorning") == 100
+
+def test_value_startsWith_h():
+    assert value("how are you") == 20
+    assert value("how's it going") == 20
+    assert value("have you been?") == 20
+
+def test_value_no_h():
+    assert value("Good morning") == 100
+    assert value("What's up") == 100
+    assert value("welcome") == 100
