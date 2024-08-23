ba203ea9798acee93efb77bd01bfca3937e64609
HernandezPy <douglasore14@gmail.com>
2024-08-23 15:48:35 +0000

    Fri, Aug 23, 2024, 11:48 AM -04:00

--- plates/test_plates.py
+++ plates/test_plates.py
@@ -4,10 +4,13 @@ def test_number_placement():
     assert is_valid("aad13") == True
 
 def test_zero_placement():
-    assert is_valid("@dd14") == False
+    assert is_valid("ddd140") == False
 
 def test_beginning_alphabetical():
     assert is_valid("aei") == True
 
 def test_numbers_at_ends():
     assert is_valid("mmm123") == True
+
+def test_alphanumeric_characters():
+    assert is_valid(")
