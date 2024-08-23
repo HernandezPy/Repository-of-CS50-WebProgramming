a47a10ad4864526b4ff36c6a9ca5e5762d7a35f1
HernandezPy <douglasore14@gmail.com>
2024-08-23 15:42:12 +0000

    Fri, Aug 23, 2024, 11:42 AM -04:00

--- plates/test_plates.py
+++ plates/test_plates.py
@@ -2,3 +2,12 @@ from plates import is_valid
 
 def test_is_valid_two_letters():
     assert is_valid("aad13") == True
+
+def test_invalid_characters():
+    assert is_valid("@dd14") == False
+
+def test_correct_length():
+    assert is_valid("douglas14") == False
+
+def test_numbers_at_ends():
+    assert is_valid("mmm123") == True
