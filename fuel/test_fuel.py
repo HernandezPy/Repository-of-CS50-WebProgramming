53f33a302a163c60a7fa7e22f2f5981b91091184
HernandezPy <douglasore14@gmail.com>
2024-08-27 16:42:52 +0000

    Tue, Aug 27, 2024, 12:42 PM -04:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -1,10 +1,15 @@
 from fuel import get_fuel_fraction
 
+def test_right_fractions():
+    assert get_fuel_fraction("1/4") ==
+    assert get_fuel_fraction("1/2") == 50%
+    assert get_fuel_fraction(")
+
 def test_error_zero():
     with pytest.raise(ZeroDivisionError):
         get_fuel_fraction("0")
 
 def test_error():
     with pytest.raise(ValueError):
-        get_fuel_fraction(")
+        get_fuel_fraction("200/3")
 
