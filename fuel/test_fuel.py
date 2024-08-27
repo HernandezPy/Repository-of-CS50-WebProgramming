8a01349f573ee006bc77061f38fb5e7dec1deaf1
HernandezPy <douglasore14@gmail.com>
2024-08-27 17:03:16 +0000

    Tue, Aug 27, 2024, 1:03 PM -04:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -1,11 +1,11 @@
 from fuel import get_fuel_fraction
 
 def test_right_fractions():
-    assert get_fuel_fraction("0/1") == E
+    assert get_fuel_fraction("0/1") == 0
     assert get_fuel_fraction("1/4") == 25.0
     assert get_fuel_fraction("1/2") == 50.0
     assert get_fuel_fraction("3/4") == 75.0
-    assert get_fuel_fraction("4/4") == F
+    assert get_fuel_fraction("4/4") == 100.0
 
 def test_error_zero():
     assert get_fuel_fraction("0/0") == "ZeroDivionError"
