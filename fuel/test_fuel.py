1530517bde9a0954dcb04660f44c4cf6c744e7d5
HernandezPy <douglasore14@gmail.com>
2024-08-27 16:32:39 +0000

    Tue, Aug 27, 2024, 12:32 PM -04:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -4,5 +4,7 @@ def test_error_zero():
     with pytest.raise(ZeroDivisionError):
         get_fuel_fraction("0")
 
-def test_
+def test_error():
+    with pytest.raise(ValueError):
+        get_fuel_fraction(")
 
