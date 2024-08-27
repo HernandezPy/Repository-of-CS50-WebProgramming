3192ce2775d5ca219835591e872d5d2143cbc030
HernandezPy <douglasore14@gmail.com>
2024-08-27 15:53:17 +0000

    Tue, Aug 27, 2024, 11:53 AM -04:00

--- plates/test_plates.py
+++ plates/test_plates.py
@@ -1,16 +1,25 @@
 from plates import is_valid
 
 def test_starts_with_two_letters():
-    assert is_valid("aad13") == True
+    assert is_valid("aa123") == True
+    assert is_valid("A") == False
 
-def test_zero_placement():
-    assert is_valid("ddd140") == True
+def test_correct_length():
+    assert is_valid("AB") == True
+    assert is_valid("12345") == False
 
-def test_beginning_alphabetical():
-    assert is_valid("abc333") == True
+def test_no_invalid_characters():
+    assert is_valid("ABC333") == True
+    assert is_valid("AB 234") == False
 
 def test_numbers_at_ends():
-    assert is_valid("mmm123") == True
+    assert is_valid("AB123") == True
+    assert is_valid("AB12C3") == False
 
-def test_alphanumeric_characters():
-    assert is_valid("yes123")
+def test_is_valid():
+    assert is_valid("ABC123") == True
+    assert is_valid("AB") == True
+    assert is_valid("AB12c3") == False
+    assert is_valid("A12345") == False
+    assert is_valid("AB12345") == False
+    assert is_valid("AB@123") == False
