7e0f16763573c2877b5df94942683cdf7e28b1f2
HernandezPy <douglasore14@gmail.com>
2024-08-29 16:03:19 +0000

    Thu, Aug 29, 2024, 12:03 PM -04:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -3,9 +3,9 @@ from fuel import convert, gauge
 
 def test_right_fractions():
     assert convert, gauge("0/1") == E
-    assert convert, gauge("1/4") == 25.0
-    assert convert, gauge("1/2") == 50.0
-    assert convert, gauge("3/4") == 75.0
+    assert convert, gauge("1/4") == 25%
+    assert convert, gauge("1/2") == 50%
+    assert convert, gauge("3/4") == 75%
     assert convert, gauge("4/4") == F
 
 def test_error_zero():
