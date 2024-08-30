4f4fb1fe62e2baa84e022eab8ff6c49936a7b9d9
HernandezPy <douglasore14@gmail.com>
2024-08-30 16:11:50 +0000

    Fri, Aug 30, 2024, 12:11 PM -04:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -2,11 +2,11 @@ import pytest
 from fuel import convert, gauge
 
 def test_right_fractions():
-    assert gauge(convert("0/1")) == "E"
-    assert gauge(convert("1/4")) == "25%"
-    assert gauge(convert("1/2")) == "50%"
-    assert gauge(convert("3/4")) == "75%"
-    assert gauge(convert("4/4")) == "F"
+    assert gauge(convert("E")) == "0/1"
+    assert gauge(convert("25%")) == "1/4"
+    assert gauge(convert("50%")) == "1/2"
+    assert gauge(convert("75%")) == "3/4"
+    assert gauge(convert("F")) == "4/4"
 
 def test_error_zero():
     with pytest.raises(ZeroDivisionError):
