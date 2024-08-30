33584262a1bfac031d3253794b48088ffd745224
HernandezPy <douglasore14@gmail.com>
2024-08-30 16:24:00 +0000

    Fri, Aug 30, 2024, 12:24 PM -04:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -24,7 +24,7 @@ def convert(fraction):
             raise
 
 def gauge(percentage):
-       percentage = convert()
+       percentage = int(percentage)
        if percentage <= 1:
           print("E")
        elif percentage >= 99:
