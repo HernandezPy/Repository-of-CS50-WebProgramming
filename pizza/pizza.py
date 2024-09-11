1c0009444fe0302a6d51567b544f75ed0d3d09aa
HernandezPy <douglasore14@gmail.com>
2024-09-11 16:12:00 +0000

    Wed, Sep 11, 2024, 12:12 PM -04:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -5,7 +5,7 @@ import os
 def pinocho_menu(filename):
     try:
         with open(filename, "r") as file:
-            file = filename.reader(filename)
+            file = csv.reader(csvfile)
     except FileNotFoundError:
         sys.exit("File not Found")
 
