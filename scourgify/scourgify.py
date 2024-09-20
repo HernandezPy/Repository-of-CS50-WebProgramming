614ef1f1270b710f5663b834f7e9a8ee2052dcf9
HernandezPy <douglasore14@gmail.com>
2024-09-20 16:18:37 +0000

    Fri, Sep 20, 2024, 12:18 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,6 +5,7 @@ import os
 def main():
     if len(sys.argv) != 2:
         sys.exit("Too few commnand-lines arguments)
+                 
     filename = sys.argv[1]
 
     if not filename.endswith(".csv"):
