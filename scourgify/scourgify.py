48decf55b5328afdee482767a61d51ff65228302
HernandezPy <douglasore14@gmail.com>
2024-09-20 16:17:47 +0000

    Fri, Sep 20, 2024, 12:17 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,13 +2,16 @@ import csv
 import sys
 import os
 
-def main()"
+def main():
     if len(sys.argv) != 2:
         sys.exit("Too few commnand-lines arguments)
     filename = sys.argv[1]
 
     if not filename.endswith(".csv"):
         sys.exit("Not a CSV file")
+
+    if not os.path.isfile(filename):
+        sys.exit("File does Not exist")
 
 
 with open("before.csv", "r") as file:
