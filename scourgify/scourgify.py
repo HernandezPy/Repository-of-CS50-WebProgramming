b55a5068cde968bc9d1567b0d36a9d51ed7958fd
HernandezPy <douglasore14@gmail.com>
2024-09-20 16:33:21 +0000

    Fri, Sep 20, 2024, 12:33 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,4 +1,4 @@
 with open("before.csv",) as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print({row[0]}, {row[1]},)
+        name, house = line.rstrip().split(",")
+        print(f"{name}, {house}")
