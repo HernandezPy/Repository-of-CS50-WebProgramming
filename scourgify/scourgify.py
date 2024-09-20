50a892779d8d0e80d4879c1105be1308ec2cd898
HernandezPy <douglasore14@gmail.com>
2024-09-20 16:12:21 +0000

    Fri, Sep 20, 2024, 12:12 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,7 +2,10 @@ import csv
 import sys
 
 def main()"
-    if len()
+    if len(sys.argv) != 2:
+        sys.exit("Too few commnand-lines arguments)
+
+
 with open("before.csv", "r") as file:
     for row in file:
         print(row.rstrip())
