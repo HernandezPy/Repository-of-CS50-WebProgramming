f1c61792ffdfb95cc60b43e4c46844825e5abf93
HernandezPy <douglasore14@gmail.com>
2024-09-20 16:46:49 +0000

    Fri, Sep 20, 2024, 12:46 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,4 +1,13 @@
+rows = []
+
 with open("before.csv") as file:
     for row in file:
-        print(row.rstrip().split(','))
+        name, house = row.rstrip()
+        row = {"name": name, "house": house}
+        rows.append(row)
+
+for row in sorted(rows):
+    print(f"{row['name'], row['hose']}")
+
+
 
