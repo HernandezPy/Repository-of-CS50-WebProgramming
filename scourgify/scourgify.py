fbf7bdfc7821d8f12b0082d12873362425b5538a
HernandezPy <douglasore14@gmail.com>
2024-09-20 16:28:58 +0000

    Fri, Sep 20, 2024, 12:28 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,5 +1,4 @@
-def scourgify():
-        with open("before.csv",) as file:
-            for line in file:
-                 row = line.rstrip().split(",")
-                 print(f"{row[0]}, {row[1]}")
+with open("before.csv",) as file:
+    for line in file:
+        row = line.rstrip().split(",")
+        print(f"{row[0]}, {row[1]}")
