45639da0458b62b0aa9299fb093fa2e76ec982b2
HernandezPy <douglasore14@gmail.com>
2024-10-01 15:19:21 +0000

    Tue, Oct 1, 2024, 11:19 AM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,7 +3,7 @@ import csv
 def main():
     with open("before.csv", "r") as before, open("after.csv", "w") as after:
         reader = csv.DictReader(before)
-        writer = csv.DictWriter(after)
+        writer = csv.DictWriter(after, fieldnames= )
         for row in reader:
             print(row)
 
