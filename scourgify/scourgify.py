9d60aee232d82de27061359921236dee56726542
HernandezPy <douglasore14@gmail.com>
2024-10-01 15:12:43 +0000

    Tue, Oct 1, 2024, 11:12 AM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -4,6 +4,6 @@ def main():
     with open("before.csv", "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            print(row)
+            print(row["name"])
 
 main()
