11cfc0297627d731505bf8ceba1aaf28e0020dfd
HernandezPy <douglasore14@gmail.com>
2024-10-01 14:45:46 +0000

    Tue, Oct 1, 2024, 10:45 AM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,4 +1,18 @@
 import csv
+import sys
+import os
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Too few command-line arguments")
+
+    filename = sys.argv[1]
+
+    if not filename.endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+    if not os.path.isfile(filename)
+        sys.exit("file not found")
 
 with open("before.csv", "r") as file:
     reader = file.read()
