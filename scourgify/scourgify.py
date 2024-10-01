31bfe1fcf57a6c5b6f8a6149da17000c75f2066d
HernandezPy <douglasore14@gmail.com>
2024-10-01 16:13:34 +0000

    Tue, Oct 1, 2024, 12:13 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,6 +1,6 @@
 import csv
 
-def main():
+def correct_file():
     with open("before.csv", "r") as before, open("after.csv", "w", newline='') as after:
         reader = csv.DictReader(before)
         writer = csv.DictWriter(after, fieldnames=["FirstName", "LastName", "House"])
@@ -17,4 +17,20 @@ def main():
             )
 
 
-main()
+def main():
+    if len(sys.argv) != 3:
+        sys.exit("Too few command-line arguments")
+    else:
+        sys.exit("Too many commnad-line arguments")
+    filename = sys.argv[1]
+
+    if not filename.endswith(".csv"):
+        sys.exit("Not a CSV file")
+
+    if not os.path.isfile(filename):
+        sys.exit("File does not exist")
+
+    correct_file()
+
+if __name__ == "__main__":
+    main()
