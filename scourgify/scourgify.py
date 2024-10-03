abdfe86835a1919903d58bdd0690d0c8f0e5d0d8
HernandezPy <douglasore14@gmail.com>
2024-10-03 15:25:05 +0000

    Thu, Oct 3, 2024, 11:25 AM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -6,4 +6,4 @@ with open("before.csv") as file:
     reader = csv.reader(file)
     for name, house in reader:
         before.append({"name": name, "home": house})
-        print(f"{s})
+        print(f"{['name']}, {['house']})
