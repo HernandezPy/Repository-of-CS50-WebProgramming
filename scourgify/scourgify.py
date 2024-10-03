81936a4a944966031c03b0d7240164d40b0293f1
HernandezPy <douglasore14@gmail.com>
2024-10-03 15:54:50 +0000

    Thu, Oct 3, 2024, 11:54 AM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,13 +1,32 @@
+import sys
 import csv
 
+if len(sys.argv) < 3:
+    sys.exit("Too few commnand-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
 
-with open("before.csv", "w") as file:
-    fieldnames = ["first", "last", "house"]
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
 
-    writer.writeheader()
+input_file = sys.argv[1]
+output_file = sys.argv[2]
 
-    for row in fieldnames:
-        last_name, first_name = row["name"].split(", ")
-        writer.writerow({"first": first_name, "last": last_name, "house": row["house"]})
-        print(fieldnames)
+try:
+    with open(input_file, "r") as infile:
+        reader = csv.DictReader(infile)
+
+        if 'name' not in reader.fieldnames or 'house' not in reader.fieldnames:
+            sys.exit("Not a CSV file")
+
+        with open(output_file, 'w', newline='') as outfile:
+            fieldnames = ['first', 'last', 'house']
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+
+            for row in reader:
+                last_name, first_name = row['name'].split(', ')
+                writer.writerow({'first': first_name, 'last': last_name, 'house': row['house']})
+
+except FileNotFoundError:
+    sys.argv(f'Could not read {input_file}')
+
