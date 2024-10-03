351736c8351367c02f3993ef6885365b7b68a86e
HernandezPy <douglasore14@gmail.com>
2024-10-03 15:19:51 +0000

    Thu, Oct 3, 2024, 11:19 AM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,8 +1,8 @@
 import csv
 
 with open("before.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row)
+    reader = csv.reader(file)
+    for name, house in reader:
+        before.append
 
 
