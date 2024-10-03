51eea8c626f5a1d117316eccb4078f7ce47d989c
HernandezPy <douglasore14@gmail.com>
2024-10-03 15:48:05 +0000

    Thu, Oct 3, 2024, 11:48 AM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,7 +1,14 @@
 import csv
 
 
-with open("before.csv") as file:
-    reader = csv.reader(file)
+with open("before.csv" "w") as file:
+    reader = csv.DictReader(file)
+    fieldnames = ["first", "last", "house"]
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    writer.writeheader()
+
     for row in reader:
-        print(row)
+        last_name, first_name = row["name"].split(", ")
+        writer.writerow({"first": first_name, "last": last_name, "house": row["house"]})
+        
