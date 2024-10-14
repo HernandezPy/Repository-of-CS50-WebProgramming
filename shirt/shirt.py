6e682fc854de263a0d6f29860ece7913d5ced436
HernandezPy <douglasore14@gmail.com>
2024-10-14 02:04:50 +0000

    Sun, Oct 13, 2024, 10:04 PM -04:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -8,20 +8,38 @@ def main():
     elif len(sys.orgv) > 3:
         sys.exit("Too many command-line arguments")
 
-input_path = sys.argv[1]
-output_path = sys.argv[2]
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
 
-valid_extensions = [".jpg", ".jpeg", ".png"]
-input_ext = os.path.splitext(input_path)[1].lower()
-output_ext = os.path.splitext(output_path)[2].lower()
+    valid_extensions = [".jpg", ".jpeg", ".png"]
+    input_ext = os.path.splitext(input_path)[1].lower()
+    output_ext = os.path.splitext(output_path)[2].lower()
 
-if input_ext not in valid_extensions or output_ext not in valid_extensions:
-    sys.exit("Invalid output")
-if input_ext != output_ext:
-    sys.exit("Input and Output have different extensions")
-if not os.path.isfile(input_path):
-    sys.exit("Input does not exist")
+    if input_ext not in valid_extensions or output_ext not in valid_extensions:
+        sys.exit("Invalid output")
 
+    if input_ext != output_ext:
+        sys.exit("Input and Output have different extensions")
+
+    if not os.path.isfile(input_path):
+        sys.exit("Input does not exist")
+
+
+try:
+    input_img = Image.open(input_path)
+    shirt_img = Image.open("shirt.png")
+
+    resized_img = ImageOps.fit(input_img, size)
+    resized_img.paste(shirt_img, shirt_img)
+    resized_img.save(output_path)
+
+except FileNotFound:
+    sys.exit("File not found")
+except Exception as e:
+    sys.exit(f"Error: {e})
+
+if __name__ == "__main__":
+    main()
 
 
 
