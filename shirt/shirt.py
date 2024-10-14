13d8f60a7cc9ab0e98053f71174d96019b89a7fb
HernandezPy <douglasore14@gmail.com>
2024-10-14 01:49:36 +0000

    Sun, Oct 13, 2024, 9:49 PM -04:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -12,7 +12,7 @@ output_path = sys.argv[2]
 
 valid_extensions = [".jpg", ".jpeg", ".png"]
 input_ext = os.path.splitext(input_path)[1].lower()
-imput_ext = os.path.splitext(output_path)[2].lower()
+output_ext = os.path.splitext(output_path)[2].lower()
 
 if input_ext not in valid_extensions or output_ext not in valid_extensions:
     sys.exit("Invalid output")
