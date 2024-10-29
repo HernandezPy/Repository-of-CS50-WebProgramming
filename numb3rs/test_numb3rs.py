d535342a02f41ca48831998e91708bb8dc6da26c
HernandezPy <douglasore14@gmail.com>
2024-10-29 15:32:49 +0000

    Tue, Oct 29, 2024, 11:32 AM -04:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,5 +1,22 @@
-import numb3rs
+import pytest
+from numb3rs import validate
 
 
-def test_validate(ip):
-    suffert validate
+def test_validate():
+    assert validate("1.1.1.1") == True
+    assert validate("255.198.1.3") == True
+    assert validate("1.2.3.4") == True
+    assert validate("255.255.255.255") == True
+
+
+def test_no_validate():
+    assert validate("300,300,200,333") == False
+    assert validate("1:2:3:3") == False
+    assert validate("cat") == False
+
+
+def test_range():
+    assert validate("260.3.23.12") == False
+    assert validate("1.256.2.124") == False
+    assert validate("23.122.300.0") == False
+    assert validate("1.2.3.1000") == False
