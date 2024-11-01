5bbbf87b185bcffeef1c403aa25f6f41ae0c9e41
HernandezPy <douglasore14@gmail.com>
2024-11-01 15:27:58 +0000

    Fri, Nov 1, 2024, 11:27 AM -04:00

--- working/test_working.py
+++ working/test_working.py
@@ -16,6 +16,7 @@ def lower_case_time():
 def incorrect_time():
     assert convert("09:00 AM to 17:00 PM") == ("ValueError")
     assert convert("3:60 AM to 9:69 PM") == ("ValueError")
+    assert convert(")
 
 def wrong_characters():
     assert convert("9:00 AM - 7:00 PM") == ("ValueError")
