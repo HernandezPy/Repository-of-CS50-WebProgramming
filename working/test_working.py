182d496ffadbeb0c41d214ed8f9c2ba8f8538cfb
HernandezPy <douglasore14@gmail.com>
2024-11-01 14:59:50 +0000

    Fri, Nov 1, 2024, 10:59 AM -04:00

--- working/test_working.py
+++ working/test_working.py
@@ -4,3 +4,5 @@ from working import convert
 
 def test_correct_time():
     assert convert("9:00 AM to 5:00 PM") == ("09:00 to 17:00")
+    assert convert("7:00 AM to 3:00 PM") == ("07:00 to 15:00")
+    assert convert(")
