b34a069a444e3c46c86ff4b1258dac12f82bf10d
HernandezPy <douglasore14@gmail.com>
2024-11-05 16:07:36 +0000

    Tue, Nov 5, 2024, 11:07 AM -05:00

--- um/test_um.py
+++ um/test_um.py
@@ -8,17 +8,13 @@ def test_count_mixed_case()
     assert count("Um, uM, UM") == 3
 
 def test_count_no_um():
-    # Test case where "um" does not appear at all
     assert count("hello world") == 0
 
 def test_count_part_of_other_word():
-    # Test case where "um" is part of another word
     assert count("yummy") == 0
 
 def test_count_punctuation():
-    # Test case with punctuation around "um"
     assert count("um...um?um!") == 3
 
 def test_count_empty_string():
-    # Test case with an empty string
     assert count("") == 0
