dad68c85868a2e7e24ab6816953195b894f5d735
HernandezPy <douglasore14@gmail.com>
2024-11-06 17:57:58 +0000

    Wed, Nov 6, 2024, 12:57 PM -05:00

---
+++ response/response.py
@@ -0,0 +1,17 @@
+import validators
+
+
+def main():
+    email = input("Enter your Email: ").strip()
+    print(valid_email, email)
+
+
+def valid_email(email):
+    if validators.email(email):
+        return "Valid"
+    else:
+        return "Invalid"
+
+
+if __name__ == "__main__":
+    main()
