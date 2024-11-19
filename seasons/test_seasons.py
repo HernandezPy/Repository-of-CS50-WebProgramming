830347ceabf2127dc5665b4fcd3b6166200efa12
HernandezPy <douglasore14@gmail.com>
2024-11-19 02:38:34 +0000

    Mon, Nov 18, 2024, 9:38 PM -05:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,15 +1,7 @@
-import pytest
 from seasons import check_correct_date
-
-def main():
-    check_correct_date()
-
+import pytest
 
 def check_correct_date(birthday):
     assert check_correct_date("1995-02-14") == ("1995, 02, 14")
     assert check_correct_date("1995-2-14") == None
     assert check_correct_date("November 5, 2014") == None
-
-
-if __name__ == "__main__":
-    main()
