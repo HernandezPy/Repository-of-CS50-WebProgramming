45ee40977be73c91dfc8d3ea5a2bafec302b2bf1
HernandezPy <douglasore14@gmail.com>
2024-11-20 17:36:02 +0000

    Wed, Nov 20, 2024, 12:36 PM -05:00

---
+++ shirtificate/shirtificate.py
@@ -0,0 +1,18 @@
+from fpdf import FPDF
+
+
+def main():
+    name_on_tshirt = input("Name: ")
+
+
+def pdf_tshirt():
+    pdf = FPDF(orientation="portrait", format="A4", width=210, height=297)
+    pdf.add_page()
+    pdf.image("shirtificate.png", alt_text="name_on_tshirt")
+    pdf.set_font("helvetica", size=12)
+    pdf.text(text="CS50 Shirtificate")
+    pdf.output("HelloWord.pdf")
+
+
+if __name__ == '__main__':
+    main()
