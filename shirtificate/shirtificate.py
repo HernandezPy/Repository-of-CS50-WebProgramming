04e9db2dc9421324ade6384a6ab44e9315bd7c17
HernandezPy <douglasore14@gmail.com>
2024-11-20 18:25:00 +0000

    Wed, Nov 20, 2024, 1:25 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,16 +5,15 @@ class PDF(FPDF):
     def __init__(self, name):
         self._pdf = FPDF()
         self._pdf.add_page()
-        self._pdf.set_font("Arial", size=40)
+        self._pdf.set_font("Arial", "B", size=40)
         self._pdf.cell(0, 60, "CS50 Shirtificate", new_x="LMARGIN", align="C")
         self._pdf.image("shirtificate.png", w=self._pdf.eph)
-        self._pdf.set_font(30)
-        self._pdf.set_text_color("blue")
+        self._pdf.set_font_size(30)
+        self._pdf.set_text_color(255, 255, 255)
         self._pdf.text(x=47.5, y=140, txt=f"{name} took CS50")
 
     def save(self, name):
-        self._pdf.save
-
+        self._pdf.output(name)
 
 name = input("Nane: ")
 pdf = PDF(name)
