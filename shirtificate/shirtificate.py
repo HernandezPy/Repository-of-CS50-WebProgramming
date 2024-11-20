9e306b231dd8ab7f1079c01087cdd7d104982129
HernandezPy <douglasore14@gmail.com>
2024-11-20 17:43:44 +0000

    Wed, Nov 20, 2024, 12:43 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -11,7 +11,7 @@ def pdf_tshirt():
     pdf.image("shirtificate.png", alt_text="name_on_tshirt")
     pdf.set_font("helvetica", size=12)
     pdf.text(text="CS50 Shirtificate")
-    pdf.output("shirtificate.pdf")
+    pdf.save("shirtificate.pdf")
 
 
 if __name__ == '__main__':
