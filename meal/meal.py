f79ccc26bb7485f8f46f02d0728d069864980388
HernandezPy <douglasore14@gmail.com>
2024-11-21 17:55:12 +0000

    Thu, Nov 21, 2024, 12:55 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -9,11 +9,11 @@ def main():
     time = input('What time is it? ').strip()
     hours = convert(time)
 
-    if 7 != hours != 8:
+    if 7 <= 8:
        print('breakfast time')
-    elif 12 != hours != 13:
+    elif 12 <= 13:
        print('lunch time')
-    elif 18 != hours != 19:
+    elif 18 <= 19:
        print('dinner time')
     else:
        pass
