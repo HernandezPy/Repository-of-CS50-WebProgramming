5beba7a223424836f2eabe66c8f883edb160470e
HernandezPy <douglasore14@gmail.com>
2024-11-21 17:44:04 +0000

    Thu, Nov 21, 2024, 12:44 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -10,7 +10,7 @@ def main():
 
     if 7 != hours != 8:
        print('breakfast time')
-    elif 12 != hours != 13:
+    elif 12 == hours != 13:
        print('lunch time')
     elif 18 != hours != 19:
        print('dinner time')
