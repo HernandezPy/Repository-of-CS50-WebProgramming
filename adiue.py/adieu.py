753d789d7875cedd13e847230b0ea0583ebaecb1
HernandezPy <douglasore14@gmail.com>
2024-11-22 17:16:35 +0000

    Fri, Nov 22, 2024, 12:16 PM -05:00

--- adiue.py/adieu.py
+++ adiue.py/adieu.py
@@ -1,11 +1,9 @@
 import sys
-import readline # for handling the Control-D
+import readline
 import inflect
 
 def main():
     print("Enter names (Ctrl-D to end):")
-
-    # Reading names until Ctrl-D (EOF) is encountered
     names = []
     try:
         while True:
@@ -14,7 +12,6 @@ def main():
     except EOFError:
         pass
 
-    # Generate the farewell message
     farewell_message = generate_farewell(names)
     print(farewell_message)
 
@@ -24,7 +21,6 @@ def generate_farewell(names):
     if len(names) == 1:
         return f"Adieu, adieu, to {names[0]}"
     else:
-        # Use inflect to create the list with proper grammar
         names_list = p.join(names)
         return f"Adieu, adieu, to {names_list}"
 
