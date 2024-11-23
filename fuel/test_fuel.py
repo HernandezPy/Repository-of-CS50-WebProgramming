fb49d3044ff9ffca653188003c6b1c2c881d4bd1
HernandezPy <douglasore14@gmail.com>
2024-11-23 18:00:30 +0000

    Sat, Nov 23, 2024, 1:00 PM -05:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -35,8 +35,8 @@ def test_gauge():
     assert gauge(50) == "50%"
     assert gauge(75) == "75%"
     assert gauge(25) == "25%"
-    assert gauge(99/100) == "F"
-    assert gauge(1/100) == "E"
+    assert gauge(99) == "F"
+    assert gauge(1) == "E"
 
 
 
