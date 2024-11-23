bd1e10c31a9a8909c53ab07099b91cb6c38d364e
HernandezPy <douglasore14@gmail.com>
2024-11-23 17:27:38 +0000

    Sat, Nov 23, 2024, 12:27 PM -05:00

--- fuel/test_fuel.py
+++ fuel/test_fuel.py
@@ -6,7 +6,7 @@ def test_convert():
     assert convert("3/4") == 75.0 and gauge(75) == "75%"
     assert convert("1/4") == 25.0 and gauge(25) == "25%"
     assert convert("4/4") == 100.0 and gauge(100) == "F"
-    assert convert("0/0") == 0.0 and gauge(0) == "E"
+    assert convert("0/1") == 0.0 and gauge(0) == "E"
 
 def test_zero_division():
     with pytest.raises(ZeroDivisionError):
