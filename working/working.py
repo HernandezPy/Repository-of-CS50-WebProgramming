51d42848339a7bcc3089ca5cecdaa7e50cd77847
HernandezPy <douglasore14@gmail.com>
2024-11-24 17:44:27 +0000

    Sun, Nov 24, 2024, 12:44 PM -05:00

--- working/working.py
+++ working/working.py
@@ -13,7 +13,7 @@ def main():
 
 def convert(s):
     pattern = r"^(\d{1,2})(?::(\d{1,2}))? (AM|PM) to (\d{1,2})(?::(\d{1,2}))? (AM|PM)$"
-    match = re.match(pattern, s)
+    match = re.search(pattern, s)
 
     if not match:
         raise ValueError("ValueError")
