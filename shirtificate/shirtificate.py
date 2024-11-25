3b44404b4202e44d46387b7b419a22bc788bd5d6
HernandezPy <douglasore14@gmail.com>
2024-11-25 16:07:04 +0000

    Mon, Nov 25, 2024, 11:07 AM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,7 +6,7 @@ class PDF(FPDF):
         self._pdf = FPDF(orientation="P", format="A4")
         self._pdf.add_page()
         self._pdf.set_font("Arial", "B", size=40)
-        self._pdf.cell(0, 20, "CS50 Shirtificate", ln=True, align="C")
+        self._pdf.cell(0, 60, "CS50 Shirtificate", ln=True, align="C")
         self._pdf.image("shirtificate.png", x=15, y=60, w=180)
         self._pdf.set_font("Arial", "B", 25)
         self._pdf.set_text_color(255, 255, 255)
