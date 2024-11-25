8e280afc4480667bc27a1e8156ded4f75fea5999
HernandezPy <douglasore14@gmail.com>
2024-11-25 16:34:06 +0000

    Mon, Nov 25, 2024, 11:34 AM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -11,10 +11,10 @@ class PDF(FPDF):
         self._pdf.image("shirtificate.png", x=15, y=60, w=180)
         self._pdf.set_font("Arial", "B", 25)
         self._pdf.set_text_color(255, 255, 255)
-        self._pdf.text = f"{self.name} took CS50"
-        self._pdf.text_width = self.get_string_width(self.text)
-        self._pdf.page_width = self.w
-        self._pdf.text(page_width - text_width) / 2
+        text = f"{self.name} took CS50"
+        text_width = self.get_string_width(self.text)
+        page_width = self.w
+        x_position = (page_width - text_width) / 2
         self._pdf.text(x=x_position, y=140, txt= text)
         self._pdf.output("shirtificate.pdf")
 
