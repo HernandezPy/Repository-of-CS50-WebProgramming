b1b233e86168e0557bb1b34bdad2efc6b0b7f600
HernandezPy <douglasore14@gmail.com>
2024-11-25 15:49:33 +0000

    Mon, Nov 25, 2024, 10:49 AM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,7 +5,7 @@ class PDF(FPDF):
     def __init__(self, name):
         self._pdf = FPDF(orientation="P", format="A4")
         self._pdf.add_page()
-        self._pdf.set_font("Arial", "B", size=20)
+        self._pdf.set_font("Arial", "B", size=40)
         self._pdf.cell(0, 20, "CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", align="C")
         self._pdf.image("shirtificate.png", w=self._pdf.epw)
         self._pdf.set_font_size(23)
