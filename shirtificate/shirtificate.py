d0ac435bbdae83d1385e6c9634734e564b727039
HernandezPy <douglasore14@gmail.com>
2024-11-25 15:47:32 +0000

    Mon, Nov 25, 2024, 10:47 AM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -10,7 +10,7 @@ class PDF(FPDF):
         self._pdf.image("shirtificate.png", w=self._pdf.epw)
         self._pdf.set_font_size(23)
         self._pdf.set_text_color(255, 255, 255)
-        self._pdf.text(x=47.5, y=140, txt=f"{name} took CS50")
+        self._pdf.text(x=40, y=70, txt=f"{name} took CS50")
         self._pdf.output("shirtificate.pdf")
 
 name = input("Name: ")
