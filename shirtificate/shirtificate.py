eea97f502b1508d22604cc454e1223b17546f2ee
HernandezPy <douglasore14@gmail.com>
2024-11-25 16:42:07 +0000

    Mon, Nov 25, 2024, 11:42 AM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -8,9 +8,9 @@ class PDF(FPDF):
         self._pdf.set_font("Arial", "B", size=40)
         self._pdf.cell(0, 60, "CS50 Shirtificate", ln=True, align="C")
         self._pdf.image("shirtificate.png", x=15, y=60, w=180)
-        self._pdf.set_font("Arial", "B", 25)
+        self._pdf.set_font_size("Arial", "B", 25)
         self._pdf.set_text_color(255, 255, 255)
-        self._pdf.set_text(x=55, y=130, txt=f"{self.name} took CS50")
+        self._pdf.text(x=55, y=130, txt=f"{self.name} took CS50")
         self._pdf.output("shirtificate.pdf")
 
 name = input("Name: ")
