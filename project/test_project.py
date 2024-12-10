8e2f978a0b7be9761e4c89e7828fe75006232031
HernandezPy <douglasore14@gmail.com>
2024-12-10 02:44:14 +0000

    Mon, Dec 9, 2024, 9:44 PM -05:00

--- project/test_project.py
+++ project/test_project.py
@@ -4,17 +4,19 @@ from project import table_of_information, job_cost, discount_in_advance
 
 def test_table_of_information():
     output = table_of_information(data)
-    expected_output = [
-        "a: Full Mouth Restoration, 250",
-        "b: Dental Implants, 50",
-        "c: Teeth Whitening, 100",
-        "d: General Dentistry, 150",
-        "e: Crown and Bridgework, 75",
-    ]
+    expected_output = {
+
+        "a": ("Full Mouth Restoration", 250),
+        "b": ("Dental Implants", 50),
+        "c": ("Teeth Whitening", 100),
+        "d": ("General Dentistry", 150),
+        "e": ("Crown and Bridgework", 75),
+    }
+
     assert output == expected_output
 
 
-def test_job_cost():
+def test_selected_job():
     assert job_cost("a") == "Full Mouth Restoration, 250"
     assert job_cost("b") == "Dental Implants, 50"
     assert job_cost("c") == "Teeth Whitening, 100"
