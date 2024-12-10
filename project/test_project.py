566e72cb728240fc4eea158655a7a9afc27efd87
HernandezPy <douglasore14@gmail.com>
2024-12-10 03:15:47 +0000

    Mon, Dec 9, 2024, 10:15 PM -05:00

--- project/test_project.py
+++ project/test_project.py
@@ -2,27 +2,46 @@ import pytest
 from project import table_of_information, job_cost, discount_in_advance
 
 
+data = {
+    "a": ("Full Mouth Restoration", 250),
+    "b": ("Dental Implants", 50),
+    "c": ("Teeth Whitening", 100),
+    "d": ("General Dentistry", 150),
+    "e": ("Crown and Bridgework", 75),
+}
+
+
 def test_table_of_information():
     output = table_of_information(data)
-    expected_output = {
-
-        "a": ("Full Mouth Restoration", 250),
-        "b": ("Dental Implants", 50),
-        "c": ("Teeth Whitening", 100),
-        "d": ("General Dentistry", 150),
-        "e": ("Crown and Bridgework", 75),
-    }
-
+    expected_output = [
+        "Customer Services Available",
+        "a: Full Mouth Restoration - $250",
+        "b: Dental Implants - $50",
+        "c: Teeth Whitening - $100",
+        "d: General Dentistry - $150",
+        "e: Crown and Bridgework - $75"
+    ]
     assert output == expected_output
 
 
-def test_selected_job():
-    assert job_cost("a") == 250
-    assert job_cost("b") == 50
-    assert job_cost("c") == 100
+def test_job_cost():
+    assert job_cost(data, "a") == 250
+    assert job_cost(data, "b") == 50
+    assert job_cost(data, "c") == 100
+    assert job_cost(data, "f") == 0
 
 
 def test_discount_in_advance():
-    assert discount_in_advance("a") == 150.0
-    assert discount_in_advance("b") == 25.0
-    assert discount_in_advance("c") == 50.0
+    message, discounted_total = discount_in_advance(data, "a", True)
+    assert message == "Your amount to pay with discount is: $125.00"
+    assert discounted_total == 125.0
+
+
+    message, full_total = discount_in_advance(data, "a", False)
+    assert message == "You have to pay full amount: $250.00"
+    assert full_total == 250.0
+
+
+    message, total = discount_in_advance(data, "f", True)
+    assert message == "Invalid option"
+    assert total == 0
