d694a67e4e2a80b4a51814ec34ac93965500294c
HernandezPy <douglasore14@gmail.com>
2025-12-01 20:53:38 +0000

    Mon, Dec 1, 2025, 3:53 PM -05:00

--- HTML,CSS/Django/lecture3/wiki/encyclopedia/urls.py
+++ HTML,CSS/Django/lecture3/wiki/encyclopedia/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-    path("entries", views.entries, name="entries")
+    path("", views.index, name="index"),
+    path("", views.entries, name="entries")
 ]
