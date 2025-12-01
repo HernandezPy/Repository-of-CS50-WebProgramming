bcd284709bd5191d0119bb059bdcb388b5154e0a
HernandezPy <douglasore14@gmail.com>
2025-12-01 21:14:12 +0000

    Mon, Dec 1, 2025, 4:14 PM -05:00

--- HTML,CSS/Django/lecture3/wiki/encyclopedia/views.py
+++ HTML,CSS/Django/lecture3/wiki/encyclopedia/views.py
@@ -18,11 +18,11 @@ def index(request):
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entry": util.get_entry()
+        "entry": util.get_entry(tittle)
     })
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "save": util.save_entry()
+        "save": util.save_entry(tittle, content)
     })
