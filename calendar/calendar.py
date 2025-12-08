3facbbee76e9f691bdf1b593531d73b9048b4916
HernandezPy <douglasore14@gmail.com>
2025-12-08 23:37:30 +0000

    Mon, Dec 8, 2025, 6:37 PM -05:00

---
+++ calendar/calendar.py
@@ -0,0 +1,10 @@
+from calendar import *
+
+
+year = int(input("Enter Year"))
+print(calendar(year, 2, 1, 8,3))
+
+#2 = 2 characters for days (Mo, tu, etc)
+#1 = 1 line (row) for each week
+#8 = 8 rows for each month
+
