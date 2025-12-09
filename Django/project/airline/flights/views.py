f7ba916b9d64d9e5a202ac4d45def154ead18f05
HernandezPy <douglasore14@gmail.com>
2025-12-09 18:12:27 +0000

    Tue, Dec 9, 2025, 1:12 PM -05:00

--- Django/project/airline/flights/views.py
+++ Django/project/airline/flights/views.py
@@ -9,4 +9,8 @@ def index(request):
 
 
 def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
 
