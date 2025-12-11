8887ed40f62fe5c8742872e66bf0d0112c4efe12
HernandezPy <douglasore14@gmail.com>
2025-12-11 04:08:38 +0000

    Wed, Dec 10, 2025, 11:08 PM -05:00

--- Django/project/airline/flights/views.py
+++ Django/project/airline/flights/views.py
@@ -23,7 +23,7 @@ def flight(request, flight_id):
 def book(request, flight_id):
     if request.method == "POST":
         flight = Flight.objects.get(pk=flight_id)
-        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger = Passenger.objects.get(pk=(request.POST["passenger"]))
         passenger.flights.add(flight)
         return HttpResponseRedirect(reverse("flight", args=(flight.id)))
 
