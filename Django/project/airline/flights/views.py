62f499100363c20f2033518dc265bac109f48c9f
HernandezPy <douglasore14@gmail.com>
2025-12-11 04:00:58 +0000

    Wed, Dec 10, 2025, 11:00 PM -05:00

--- Django/project/airline/flights/views.py
+++ Django/project/airline/flights/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Flight
+from .models import Flight, Passenger
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
