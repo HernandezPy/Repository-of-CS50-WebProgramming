3612ad6bdcd0741f394d0be92244bd50e606c912
HernandezPy <douglasore14@gmail.com>
2025-12-11 03:57:47 +0000

    Wed, Dec 10, 2025, 10:57 PM -05:00

--- Django/project/airline/flights/models.py
+++ Django/project/airline/flights/models.py
@@ -14,13 +14,13 @@ class Flight(models.Model):
     duration = models.IntegerField()
 
     def __str__(self):
-        return f"{self.id}: {self.origin} to {self.detination}"
+        return f"{self.id}: {self.origin} to {self.destination}"
 
 
-class Passsenger(models.Model):
-    first = models.Charfield(max_length=64)
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
     last = models.CharField(max_length=64)
-    flights = models.ManyToManyField(Flight, blank=True, related_name="passergers")
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
 
     def __str__(self):
         return f"{self.first} {self.last}"
